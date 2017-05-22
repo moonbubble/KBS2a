f7fd02367cb55e41795fa4f93552c674dc5e85ba
Hielke <h.vandergugten@hotmail.com>
2017-05-22 16:28:41 +0200

    iets verdur

--- src/bpp/robot/Main.java
+++ src/bpp/robot/Main.java
@@ -3,17 +3,18 @@ package bpp.robot;
 import java.util.ArrayList;
 import java.util.List;
 
+import bpp.simulatie.algoritmes.Algoritme;
+import bpp.simulatie.algoritmes.Bibliotheek;
 import domeinmodel.Bestelling;
+import domeinmodel.Doos;
 import domeinmodel.Product;
 
 public class Main {
-	private static Product appel = new Product(1, 1, 1, 5, "appel");
+	private static Product appel = new Product(1, 1, 1, 2, "appel");
 	private static Product peer = new Product(2, 2, 1, 4, "peer");
-	private static Product banaan = new Product(3, 3, 1, 10, "peer");
-	private static Product lamp = new Product(4, 4, 1, 10, "lamp");
-	private static Product bank = new Product(5, 5, 1, 10, "peer");
-	static int paar = 1;
-	static int aantalx = 1;
+	private static Product banaan = new Product(3, 3, 1, 5, "banaan");
+	private static Product lamp = new Product(4, 4, 1, 4, "lamp");
+	private static Product bank = new Product(5, 5, 1, 2, "bank");
 
 	public static void main(String[] args) {
 
@@ -25,11 +26,7 @@ public class Main {
 		bestelling.voegProductToe(bank);
 		bestelling.getProducten();
 		bestelling.getKlant();
-
-		// Product p1 = bestelling.getProducten(0);
-		// Product p2 = bestelling.getProducten(1);
-		// Product p3 = bestelling.getProducten(2);
-
+		
 		List<Product> lijstVanProducten = new ArrayList<>(bestelling.getProducten());
 
 		Product p1 = null;
@@ -55,7 +52,22 @@ public class Main {
 			lijstVanProducten.set(lijstVanProducten.size() -1, p1);
 			lijstVanProducten.set(lijstVanProducten.size() -2, p2);
 		}
+		
+		System.out.print("VOLGORDE PRODUCTEN: ");
 		System.out.println(lijstVanProducten);
+		Algoritme algoritme = new Bibliotheek().getAlgoritme(1);
+		List<Doos> dozen = algoritme.bepaalDozen(lijstVanProducten,10);
+		
+		System.out.println("Er zijn " + dozen.size() + " dozen:");
+		
+		for(int i = 0; i < dozen.size(); i++){
+		System.out.println("In doos " + (i+1) + " zitten de volgende producten:");
+		
+//		for(Product p : dozen.getProducten()){
+//			System.out.println("artikelnr " + p);
+//		}
+		
+		}
 	}
 
 }
